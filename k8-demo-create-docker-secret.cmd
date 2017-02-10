8b07acef30b8c70808eb982b37212b9ab2bbfa5a
dave-read <davre@microsoft.com>
2017-02-10 08:23:33 -0800

    remove old files

--- k8-demo-create-docker-secret.cmd
+++
@@ -1,9 +0,0 @@
-echo off
-
-if "%5"=="" (
-    echo usage name url user password email
-    goto :end
-)
-kubectl create secret docker-registry %1 --docker-server=%2 --docker-username=%3 --docker-password=%4 --docker-email=%5
-
-:end
--- k8-demo-create-generic-secret.cmd
+++
@@ -1,9 +0,0 @@
-echo off
-
-if "%3"=="" (
-    echo usage SecretName WSID KEY
-    goto :end
-)
-kubectl create secret generic %1 --from-literal=WSID=%2 --from-literal=KEY=%3
-
-:end
